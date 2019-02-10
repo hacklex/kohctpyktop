fdac1838244d43d9698e3c74d23df46507f8db15
Дмитрий Заводской <zawodskoj@outlook.com>
2019-02-10 23:23:08 +0300

    mouse auto-release

--- Kohctpyktop/MainWindow.xaml.cs
+++ Kohctpyktop/MainWindow.xaml.cs
@@ -43,8 +43,14 @@ namespace Kohctpyktop
         {
             var pt = e.GetPosition((Image) sender);
             if (e.LeftButton == MouseButtonState.Pressed)
+            {
                 ViewModel.ProcessMouse(pt);
-            ViewModel.ProcessMouseMove(pt);
+            }
+            else
+            {
+                ViewModel.ReleaseMouse(pt);
+                ViewModel.ProcessMouseMove(pt);
+            }
         }
 
         private void ImageMouseUp(object sender, MouseButtonEventArgs e)
