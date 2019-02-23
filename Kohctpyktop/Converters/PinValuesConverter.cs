26116f146899562a2c5181b87182d5dab63d99f2
Дмитрий Заводской <zawodskoj@outlook.com>
2019-02-23 22:28:59 +0300

    change pin values layout

--- Kohctpyktop/Converters/PinValuesConverter.cs
+++ Kohctpyktop/Converters/PinValuesConverter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Kohctpyktop.Converters
@@ -34,6 +35,23 @@ namespace Kohctpyktop.Converters
             }
 
             return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+    
+    public class PinValueBorderThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is PinState state)) return null;
+
+            var lb = state.Changed ? 1 : 0;
+            
+            return state.Value
+                ? new Thickness(lb, 1, 0, 0)
+                : new Thickness(lb, 0, 0, 1);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
