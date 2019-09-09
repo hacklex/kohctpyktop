decb79b3fd51fabe27a06c7a6da9f25b96b3ace9
Thehx <thehdotx@gmail.com>
2019-09-09 04:44:51 +0300

    The second coming of Avalonia

---
+++ Kohctpyktop.Avalonia/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+using Avalonia.Controls.ApplicationLifetimes;
+namespace Kohctpyktop.Avalonia
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime)
+                lifetime.MainWindow = new MainWindow();
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ Kohctpyktop.Avalonia/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using Avalonia.Rendering;
+
+namespace Kohctpyktop.Avalonia
+{
+    class Program
+    {
+        // Initialization code. Don't use any Avalonia, third-party APIs or any
+        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+        // yet and stuff might break.
+        public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+        // Avalonia configuration, don't remove; also used by visual designer.
+        public static AppBuilder BuildAvaloniaApp()
+        {
+            var builder = AppBuilder.Configure<App>()
+                .With(new Win32PlatformOptions
+                {
+                    AllowEglInitialization = true
+                })
+                .UsePlatformDetect();
+            var wp = builder.WindowingSubsystemInitializer;
+            return builder.UseWindowingSubsystem(() =>
+            {
+                wp();
+                AvaloniaLocator.CurrentMutable.Bind<IRenderTimer>().ToConstant(new DefaultRenderTimer(500));
+            });
+        }
+
+    }
+}
